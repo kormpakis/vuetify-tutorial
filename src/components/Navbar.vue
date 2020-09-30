168bd05b8a859ef7f68853ea79d5edf9425b3e0d
Giorgos Kormpakis <korbakis@synelixis.com>
2020-09-30 09:55:25 +0300

    Initial commit.

---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav>
+    <v-app-bar flat app>
+      <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Todo</span>
+        <span>Kobra</span>
+      </v-toolbar-title>
+      <v-spacer/>
+      <v-btn text color="grey">
+        <span>Sign Out</span>
+        <v-icon right>mdi-logout</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app class="indigo">
+      <v-list>
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-action>
+            <v-icon class="white--text">{{link.icon}}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{link.text}}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'Navbar',
+    data() {
+      return {
+        drawer: false,
+        links: [
+          {icon: 'mdi-view-dashboard', text: 'Dashboard', route: '/'},
+          {icon: 'mdi-folder', text: 'My Projects', route: '/projects'},
+          {icon: 'mdi-account', text: 'Team', route: '/team'}
+        ]
+      }
+    }
+  }
+</script>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="home">
+    <h1 class="subtitle-1 grey--text">Dashboard</h1>
+    <v-container class="my-5">
+      <v-card v-for="project in projects" :key="project.title" class="pa-3" flat>
+        <v-row>
+          <v-col xs="12" sm="12" md="6">
+            <div class="caption">Project Title</div>
+            <div>{{project.title}}</div>
+          </v-col>
+          <v-col xs="5" sm="4" md="2">
+            <div class="caption">Person</div>
+            <div>{{project.person}}</div>
+          </v-col>
+          <v-col xs="5" sm="4" md="2">
+            <div class="caption">Due by</div>
+            <div>{{project.due}}1</div>
+          </v-col>
+          <v-col xs="2" sm="4" md="2">
+            <div class="caption">Status</div>
+            <div>{{project.status}}</div>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Dashboard',
+    components: {},
+    data() {
+      return {
+        projects: [
+          {
+            title: 'Design a new website',
+            person: 'The Net Ninja',
+            due: '1st Jan 2019',
+            status: 'ongoing',
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'
+          },
+          {
+            title: 'Code up the homepage',
+            person: 'Chun Li',
+            due: '10th Jan 2019',
+            status: 'complete',
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'
+          },
+          {
+            title: 'Design video thumbnails',
+            person: 'Ryu',
+            due: '20th Dec 2018',
+            status: 'complete',
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'
+          },
+          {
+            title: 'Create a community forum',
+            person: 'Gouken',
+            due: '20th Oct 2018',
+            status: 'overdue',
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'
+          },
+        ]
+      }
+    }
+  }
+</script>
